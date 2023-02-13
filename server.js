1f722b32906b1adf917d10f5626d5acf8b53051f
Dewansh Patle <dewanshpatle69@gmail.com>
2023-02-13 09:40:06 +0530

    removed build from gitignore

--- server.js
+++ server.js
@@ -3,11 +3,14 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv/config");
 const app = express();
+const path = require("path");
+
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "build")));
+
 app.use(cors());
 const authmiddleware = require("./middleware/authmiddleware");
 const PORT = process.env.PORT || 8000;
-const path = require("path");
 //Routes
 const signuproute = require("./routes/signup");
 app.use("/api/signup", signuproute);
@@ -32,8 +35,7 @@ app.get("/api", (req, res) => {
   res.send(`Hello User`);
 });
 
-app.use(express.static("./build"));
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "build", "index.html"));
 });
 
